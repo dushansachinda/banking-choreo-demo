00a365de0ebdd7326b409cb5fcf4b43505654807
dushansachinda <dushanusa81@gmail.com>
2023-01-04 20:12:59 -0800

    add web application

---
+++ web/public/runtime-config.js
@@ -0,0 +1,18 @@
+window.env = {
+    CHOREO_SERVER_URL: "https://sts.choreo.dev",
+    CHOREO_API_HOST: "https://b48cc93e-fa33-4420-a155-bc653b4d46be-dev.e1-us-east-azure.choreoapis.dev/jygq", //https://5437061a-64e8-4708-99ca-58f1efc33c5e-dev.e1-us-east-azure.choreoapis.dev/uedc",//"https://c112eada-316e-46a7-9705-df75e4a30edc-prod.e1-us-east-azure.choreoapis.dev/azyh",
+
+    CHOREO_APP_CONSUMER_KEY: "fEMkWnMknyv1BF0pbPIIajM85U8a",//"f_JWNkVgsgYHMoR77eS7OdRKlL4a",//"6NFX5ksFoExj9WOMMHsIWXWGHGoa",
+    CHOREO_APP_CONSUMER_SECRET: "M2Wuzrfk24WT2pIDTZemCGWL1yka",//"mnBEQ5Fp5mpWsDiLbflW779TJM0a",//"CfNQKLvzq_5HaioxBiSNc3TQmM4a",
+
+    CHOREO_ACCOUNT_SERVICE_CONSUMER_KEY: "SYOvdmnBsHQahysydZ7quBWZrpoa",// "2HewgYUA7ar_qUYosLISXpL9gWka",//"gpZrH0CIY9zRuyTusC8Y5RKZ4Yca",
+    CHOREO_ACCOUNT_SERVICE_SECRET: "jCnl0THf5nXvp91hORyA4kZA6vAa",//"So17BBU44Uc3epnJQP2rEe0uGxca",//"M6jcOwzG9hEbLj5mHFU_LGg9ff8a",
+
+    CHOREO_CONSENT_SERVICE_CONSUMER_KEY: "s7VQ9v5KkiHA9VueFXrvx6Vf_3oa",//"BOJIMf9t3vfrGpO5bmljmSaMTmYa",//"XU_qHB2bE5fgU0uFRz6dXnLssAQa",
+    CHOREO_CONSENT_SERVICE_SECRET: "vPxgTSQPnnO3k_3HlM1UJhp0ULUa",//"flpDi6yDADmf3ZDhw7bE0DdfsGMa",//"5WCxW2NpebUTYQzALInpCI71MY8a",
+
+    ASGARDEO_APP_CONSUMER_KEY: "RskLIFfEiB9PUOeWmMvYlHH8jsUa",//"u2dsFQERF_m9RCCA1GM6uMxsQEQa",//"fkzIBdn7WeInrcOPgOMZQt6fr2sa",
+    ASGARDEO_APP_CONSUMER_SECRET: "N99VlEMTTxeKHI4tbGg2Ghf0mxUa",//"apfbQ6oker4l0jCmTEOaRz523FAa",
+    HOST_URL: "http://localhost:3000"//"https://white-forest-06046de00.2.azurestaticapps.net",
+};
+  
